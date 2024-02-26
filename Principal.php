b6d3d8496685d6ecbff117af8430f5170408294e
mauricio1814 <alvaromauricioramirezquintero@gmail.com>
2024-02-26 18:15:32 -0500

    cambios de navbar y incluison del video y whatsapp

--- Principal.php
+++ Principal.php
@@ -8,12 +8,20 @@
     <link rel="stylesheet" href="principal1.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!--Icon-Font-->
+    <script src="https://kit.fontawesome.com/eb496ab1a0.js" crossorigin="anonymous"></script>
 </head>
 <body class="vh-100 overflow-hidden" >
+    <!-- icono whatsapp -->
+    <a href="https://wa.me/573204146958?text=Hola" class="btn-wsp" target="_blank">
+        <i class="fa fa-whatsapp icono" style="font-size: 35px;"></i>
+    </a>
+    <!-- video -->
+    <video muted autoplay loop src="img/principal.mp4" class="video"></video>
     <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg fixed-top" style="background-color: white;">
+    <nav class="navbar navbar-expand-lg fixed-top">
     <div class="container-fluid">
-        <a class="navbar-brand me-auto" href="#"> <img src="img/icono.jpg" alt="icono de la pagina"> </a>
+        <a class="navbar-brand me-auto" href="#"> <img src="img/icono.jpg" alt="icono de la pagina"  > </a>
         <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasNavbar" aria-labelledby="offcanvasNavbarLabel">
             <div class="offcanvas-header">
                 <h5 class="offcanvas-title" id="offcanvasNavbarLabel">SIALCI-SAS</h5>
@@ -22,19 +30,16 @@
             <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-center flex-grow-1 pe-3">
                     <li class="nav-item">
-                        <a class="nav-link mx-lg-2 active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link mx-lg-2 active" aria-current="page" href="#">SIALCI SAS</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mx-lg-2" href="#">About</a>
+                        <a class="nav-link mx-lg-2" href="#">Productos</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link mx-lg-2" href="#">Servicios</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link mx-lg-2" href="#">Portafolio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link mx-lg-2" href="#">contact</a>
+                        <a class="nav-link mx-lg-2" href="#">Contacto</a>
                     </li>
                 </ul>
             </div>
