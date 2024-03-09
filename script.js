2c01b3ad30bdf3daa10b59b9e741d9255fefd5a4
Abilson Rivera <abilsonriveragarcia@gmail.com>
2024-03-09 14:26:04 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,31 +8,40 @@ function validateForm() {
     let isValid = true;
   
     allInputs.forEach(input => {
-      // Si el campo no es el de "Nombre de la empresa", validarlo
-      if (input.name !== "empresa") {
-        // Verificar si el campo está vacío
-        if (input.value === "") {
-          isValid = false;
-          return; // Detener la iteración si se encuentra un campo vacío
-        }
+        // Si el campo no es el de "Nombre de la empresa", validarlo
+        if (input.name !== "empresa") {
+            // Verificar si el campo está vacío
+            if (input.value === "") {
+                isValid = false;
+                return; // Detener la iteración si se encuentra un campo vacío
+            }
   
-        // Verificar si el campo de nombre o apellido contiene números
-        if ((input.name === "nombre" || input.name === "apellidos" || input.name === "correo") && /\d/.test(input.value)) {
-          isValid = false;
-          return; // Detener la iteración si se encuentra un nombre o apellido con números
+            // Verificar si el campo de nombre, apellidos o correo contiene números
+            if ((input.name === "nombre" || input.name === "apellidos") && /\d/.test(input.value)) {
+                isValid = false;
+                return; // Detener la iteración si se encuentra un nombre, apellido o correo con números
+            }
         }
-      }
     });
 
     return isValid;
 }
 
-nextBtn.addEventListener("click", () => {
-    // Validar el formulario
+// Validar el formulario al cargar la página
+window.addEventListener('load', () => {
+    validateAndNotify();
+});
+
+// Función para validar el formulario y mostrar una alerta si es necesario
+function validateAndNotify() {
     if (!validateForm()) {
         alert("Por favor, complete todos los campos del formulario y asegúrese de que sean válidos.");
-        return; // Detener el flujo si la validación falla
     }
+}
+
+nextBtn.addEventListener("click", () => {
+    // Validar el formulario al hacer clic en el botón "Siguiente"
+    validateAndNotify();
 
     // Si la validación es exitosa, avanzar al siguiente paso
     form.classList.add('secActive');
