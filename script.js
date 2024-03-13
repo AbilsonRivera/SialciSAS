e9c62a75e1396c009cbb5b4e78cc714841bbdd49
Abilson Rivera <abilsonriveragarcia@gmail.com>
2024-03-13 15:02:37 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,51 +1,17 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const form = document.querySelector("form");
-    const nextBtn = form.querySelector(".nextBtn");
-    const backBtn = form.querySelector(".backBtn");
-    const allInputs = form.querySelectorAll("input");
-    const correoInput = form.querySelector('[name="correo"]');
+const form = document.querySelector(form),
+nextBtn = form.querySelector(".nextBtn"),
+backBtn = form.querySelector(".backBtn"),
+allInput = form.querySelectorAll(".first input");
 
-    // Función para validar el formulario y mostrar una alerta con los errores si es necesario
-    function validateAndNotify(event) {
-        event.preventDefault(); // Evitar el comportamiento predeterminado del botón "Siguiente"
-        event.stopPropagation(); // Detener la propagación del evento
-        let isValid = true;
-        const errors = [];
 
-        allInputs.forEach(input => {
-            // Si el campo no es el de "Nombre de la empresa", validarlo
-            if (input.name !== "empresa") {
-                // Verificar si el campo está vacío
-                if (input.value === "") {
-                    isValid = false;
-                    errors.push("Por favor, complete todos los campos del formulario.");
-                }
-
-                // Verificar si el campo de nombre, apellidos o correo contiene números
-                if ((input.name === "nombre" || input.name === "apellidos") && /\d/.test(input.value)) {
-                    isValid = false;
-                    errors.push("El nombre y el apellido no deben contener números.");
-                }
-            }
-
-            // Validar el correo electrónico
-            if (input.name === "correo" && !/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(input.value)) {
-                isValid = false;
-                errors.push("El correo electrónico no es válido.");
-            }
-        });
-
-        // Mostrar una alerta con los errores si es necesario
-        if (!isValid) {
-            alert(errors.join("\n"));
-        } else {
-            form.classList.add('secActive'); // Avanzar al siguiente paso si no hay errores de validación
+nextBtn.addEventListener("click", ()=>{
+    allInput.forEach(input => {
+        if(input.value != ""){
+            form.classList.add('secActive');
+        }else{
+            form.classList.remove('secActive');
         }
-    }
+    })
+})
 
-    // Evento para el botón "Siguiente"
-    nextBtn.addEventListener("click", validateAndNotify);
-
-    // Evento para el botón "Atrás"
-    backBtn.addEventListener("click", () => form.classList.remove('secActive'));
-});
+backBtn.addEventListener("click", () => form.classList.remove('secActive'));
